7b78b23bb66b552d3daf6c0e104f8991040daa88
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-16 00:30:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,13 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(file)
+  emoticons = YAML.load_file(file)
+  emoticons.each do |key, value|
+    emoticons[key] = { :english => value[0], :japanese => value[1] } 
+  end
+  emoticons
 end
 
 def get_japanese_emoticon
