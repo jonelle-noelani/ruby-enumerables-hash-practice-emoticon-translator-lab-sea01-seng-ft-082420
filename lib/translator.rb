e69b6bc97e0ec2c56cc96c56c7a1d775fea4b001
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-16 03:46:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,8 @@ def get_english_meaning(file, jmote)
       value.each do |lingo, mote|
         if new_emoticons[key][lingo] == jmote
           english_meaning = key
-        if !new_emoticons[key][lingo] == jmote
+        else
+        # if !new_emoticons[key][lingo] == jmote
           english_meaning = "Sorry, that emoticon was not found"
           end
         end
