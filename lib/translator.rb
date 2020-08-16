19f86d9112f55ca9e03d9dfa550c5afc4fbb10bf
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-16 05:37:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,6 +16,7 @@ def get_english_meaning(file, jmote)
     new_emoticons.each do |key,value|
       value.each do |lingo, mote|
         english = key[lingo][jmote]
+        binding.pry 
         if english == nil
           english = "Sorry"
         end
