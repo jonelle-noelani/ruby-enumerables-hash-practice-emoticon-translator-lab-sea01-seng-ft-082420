3b650b8a35d9a27d550a0fcc289c8a414eda0263
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-16 02:19:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,9 +13,10 @@ end
 def get_english_meaning(file, jmote)
   new_emoticons = load_library(file)
   new_emoticons.each do |key,value|
-    value.include?
-    value.each do |l_key , mote|
-      puts mote
+    value.include?jmote
+    puts key
+    # value.each do |l_key , mote|
+    #   puts mote
     # if value[key] == jmote
     # puts "Got it!"
   end
