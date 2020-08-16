82e18f191bc9b93d2a3cf7c9f1c78c5cc4003402
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-16 05:26:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,7 @@ def get_english_meaning(file, jmote)
   new_emoticons = load_library(file)
     new_emoticons.each do |key,value|
       value.each do |lingo, mote|
-        english = new_emoticons[key][lingo] == jmote ? key :"Sorry"
+        english = new_emoticons[key][lingo][jmote ? key :"Sorry"
         binding.pry
           puts english
         end
